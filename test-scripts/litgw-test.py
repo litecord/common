df49603c30650674c32e981e87b13383d7a4786a
Luna Mendes <lkmnds@gmail.com>
2018-01-14 21:44:48 -0300

    test: add heartbeating test

--- test-scripts/litgw-test.py
+++ test-scripts/litgw-test.py
@@ -8,14 +8,22 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 async def heartbeat(conn, timeout):
+    while True:
+        await conn.send(json.dumps({
+            'op': 1,
+            'd': conn._seq,
+        }))
+        await asyncio.sleep(timeout / 1000)
     pass
 
 async def main():
     logging.info('connecting')
     async with websockets.connect('ws://localhost:8080/gw') as conn:
+        conn._seq = None
         logging.info('done!')
         hello = await conn.recv()
-        hello = json.loads(hello)
+        hello = json.loads(hello)['d']
+
         loop.create_task(heartbeat(conn, hello['heartbeat_interval'] / 1000))
 
         while True:
